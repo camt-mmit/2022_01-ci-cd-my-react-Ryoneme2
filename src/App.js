ddfb9cdf6e214c89e5dcea5be9637a325c830f4c
Ryoneme2 <worknarair6@gmail.com>
2022-10-21 12:43:13 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const res = await axios.get('http://localhost:8081/data');
+        setData(res.data.data);
+        console.log(res.data.data);
+      } catch (e) {
+        console.error(e);
+        setData([]);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading</p>;
+
   return (
     <div className='App'>
       <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img
+          src='/large_thumbnail-removebg-preview.png'
+          className='App-logo'
+          alt='logo'
+        />
         <p>
-          Edit <code>src/App.js</code> and save to reloads.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className='App-link'
@@ -15,8 +41,12 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          learn react deploy docker ก็ขอนิดหนึ่งได้มั้ยล่ะ
+          Learn React
         </a>
+        {data.map((v) => {
+          console.log(v);
+          return <p key={v}>{v}</p>;
+        })}
       </header>
     </div>
   );
